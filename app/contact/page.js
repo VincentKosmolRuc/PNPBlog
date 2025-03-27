f6dcf33413cfc7b37b4ed646f193ad3913cd71bc
VincentKosmolRuc <vincent.kosmol@gmail.com>
2025-03-27 10:00:13 +0100

    forgot small thing in push

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,17 +2,17 @@ import Image from "next/image";
 
 export default function Page() {
     return (
-        <div className="grid grid-cols-1 items-center px-4">
+        <div className="grid grid-cols-1 items-center">
             <div className="justify-self-center">
                 <Image 
                     src="/VincentKosmolContact.jpg"
                     width={2000}
                     height={1000}
                     alt="Picture of Vincent Kosmol in ultra running outfit"
-                    className="w-3xl mt-10 rounded-4xl shadow-2xl"
+                    className="w-3xl md:mt-10 shadow-2xl md:rounded-4xl rounded-none"
                 />
             </div>
-            <div className="text-gray-700 mt-10 text-center"> 
+            <div className="text-gray-700 mt-10 text-center px-4"> 
                 <p className="mb-4">
                     Have a question about ultra running or one of my race reports?  
                     Want to work together?
